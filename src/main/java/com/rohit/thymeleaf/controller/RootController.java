726a7d91aac636ccda78751c93588c2e8b685ba2
Rohuu <rohuu99022@gmail.com>
2023-05-16 20:44:50 +0530

    thymeleaf fragments

--- src/main/java/com/rohit/thymeleaf/controller/RootController.java
+++ src/main/java/com/rohit/thymeleaf/controller/RootController.java
@@ -11,10 +11,26 @@ import java.util.List;
 @RequestMapping("/")
 public class RootController {
 
-    @GetMapping
+    @GetMapping("/index")
     public String index(Model model){
         model.addAttribute("pageTitle","Random_Title");
         model.addAttribute("scientists", List.of("Shubham Singh","Pulkit","Rohit","Sachin"));
         return "index";
+    }
+
+    // handler for service fragment
+    @GetMapping("/service")
+    public String serviceHandler(Model model){
+        return "service";
+    }
+
+    @GetMapping("/footer")
+    public String footerDesign(Model model){
+        return "footer";
+    }
+
+    @GetMapping("/service")
+    public String fragmentService(Model model){
+        return "service";
     }
 }
